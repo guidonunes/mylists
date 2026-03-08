5f089521161b5ca21eb9671028799aaa59cfb479
Guilherme Nunes <guilherme.augd@gmail.com>
2026-03-08 11:18:27 -0300

    feat: add signup screen boilerplate code

---
+++ app/src/main/java/br/com/fiap/mylists/screens/SignUpScreen.kt
@@ -0,0 +1,121 @@
+package br.com.fiap.mylists.screens
+
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.fiap.mylists.R
+import br.com.fiap.mylists.ui.theme.MylistsTheme
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SignupScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "My Lists",
+                fontSize = 48.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = modifier
+                    .padding(16.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.goal),
+                contentDescription = "Logo"
+            )
+        }
+        Spacer(modifier = modifier.height(48.dp))
+        Text(
+            text = "Create Account",
+            fontSize = 28.sp
+        )
+        Spacer(modifier = modifier.height(16.dp))
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text(text = "E-mail")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email
+            )
+        )
+        Spacer(modifier = modifier.height(8.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text(text = "Password")
+            },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = modifier.height(16.dp))
+        Button(
+            onClick = {}
+        ) {
+            Text(text = "Create Account")
+        }
+        Spacer(modifier = modifier.height(8.dp))
+        TextButton(
+            onClick = {}
+        ) {
+            Text("Already have an account? Enter")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SignupScreenPreview() {
+    MylistsTheme () {
+        SignupScreen(modifier = Modifier, rememberNavController())
+    }
+}
