a6fa65f6f2fe3c066f75a025f1ac646a4123e495
Guilherme Nunes <guilherme.augd@gmail.com>
2026-03-08 11:58:01 -0300

    feat: add firebase dependencies

--- app/src/main/java/br/com/fiap/mylists/screens/HomeScreen.kt
+++ app/src/main/java/br/com/fiap/mylists/screens/HomeScreen.kt
@@ -1,4 +1,89 @@
 package br.com.fiap.mylists.screens
 
-class HomeScreen {
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.fiap.mylists.ui.theme.MylistsTheme
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "My Lists",
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                },
+                colors = TopAppBarDefaults
+                    .topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary
+                    )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                shape = RoundedCornerShape(50.dp),
+                modifier = modifier
+                    .width(100.dp),
+                containerColor = MaterialTheme.colorScheme.primary
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add",
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+            Text(
+                text = "Home Screen",
+                color = MaterialTheme.colorScheme.onBackground,
+                style = MaterialTheme.typography.headlineLarge
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    MylistsTheme () {
+        HomeScreen(navController = rememberNavController())
+    }
 }
